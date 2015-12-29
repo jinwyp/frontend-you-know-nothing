57fbc314adae978b8255d68fbb1a5c386b79f635
Jin <jinwyp@gmail.com>
2015-12-29 18:55:16 +0800

    add html version

--- app/js/app.js
+++ app/js/app.js
@@ -10,7 +10,6 @@ angular.module('demoApp', ['ngMaterial', 'md.data.table'])
     .controller('tableController', ['$mdEditDialog', '$q', '$scope', '$timeout', function ($mdEditDialog, $q, $scope, $timeout) {
         'use strict';
 
-        $scope.selected = [];
 
         $scope.options = {
             rowSelection: false,
@@ -23,123 +22,161 @@ angular.module('demoApp', ['ngMaterial', 'md.data.table'])
 
         $scope.query = {
             order: 'name',
-            limit: 5,
+            limit: 500,
             page: 1
         };
 
+        $scope.selected = [];
+
+
+
+
         $scope.data = {
             "count": 9,
+            "textOptions" : [],
             "frameworkData": [
                 {
-                    "name": "Frozen yogurt",
-                    "type": "Ice cream",
-                    "calories": { "value": 159.0 },
-                    "fat": { "value": 6.0 },
-                    "carbs": { "value": 24.0 },
-                    "protein": { "value": 4.0 },
-                    "sodium": { "value": 87.0 },
-                    "calcium": { "value": 14.0 },
-                    "iron": { "value": 1.0 }
-                }, {
-                    "name": "Ice cream sandwich",
-                    "type": "Ice cream",
-                    "calories": { "value": 237.0 },
-                    "fat": { "value": 9.0 },
-                    "carbs": { "value": 37.0 },
-                    "protein": { "value": 4.3 },
-                    "sodium": { "value": 129.0 },
-                    "calcium": { "value": 8.0 },
-                    "iron": { "value": 1.0 }
-                }, {
-                    "name": "Eclair",
-                    "type": "Pastry",
-                    "calories": { "value":  262.0 },
-                    "fat": { "value": 16.0 },
-                    "carbs": { "value": 24.0 },
-                    "protein": { "value":  6.0 },
-                    "sodium": { "value": 337.0 },
-                    "calcium": { "value":  6.0 },
-                    "iron": { "value": 7.0 }
-                }, {
-                    "name": "Cupcake",
-                    "type": "Pastry",
-                    "calories": { "value":  305.0 },
-                    "fat": { "value": 3.7 },
-                    "carbs": { "value": 67.0 },
-                    "protein": { "value": 4.3 },
-                    "sodium": { "value": 413.0 },
-                    "calcium": { "value": 3.0 },
-                    "iron": { "value": 8.0 }
-                }, {
-                    "name": "Jelly bean",
-                    "type": "Candy",
-                    "calories": { "value":  375.0 },
-                    "fat": { "value": 0.0 },
-                    "carbs": { "value": 94.0 },
-                    "protein": { "value": 0.0 },
-                    "sodium": { "value": 50.0 },
-                    "calcium": { "value": 0.0 },
-                    "iron": { "value": 0.0 }
-                }, {
-                    "name": "Lollipop",
-                    "type": "Candy",
-                    "calories": { "value": 392.0 },
-                    "fat": { "value": 0.2 },
-                    "carbs": { "value": 98.0 },
-                    "protein": { "value": 0.0 },
-                    "sodium": { "value": 38.0 },
-                    "calcium": { "value": 0.0 },
-                    "iron": { "value": 2.0 }
-                }, {
-                    "name": "Honeycomb",
-                    "type": "Other",
-                    "calories": { "value": 408.0 },
-                    "fat": { "value": 3.2 },
-                    "carbs": { "value": 87.0 },
-                    "protein": { "value": 6.5 },
-                    "sodium": { "value": 562.0 },
-                    "calcium": { "value": 0.0 },
-                    "iron": { "value": 45.0 }
-                }, {
-                    "name": "Donut",
-                    "type": "Pastry",
-                    "calories": { "value": 452.0 },
-                    "fat": { "value": 25.0 },
-                    "carbs": { "value": 51.0 },
-                    "protein": { "value": 4.9 },
-                    "sodium": { "value": 326.0 },
-                    "calcium": { "value": 2.0 },
-                    "iron": { "value": 22.0 }
-                }, {
-                    "name": "KitKat",
-                    "type": "Candy",
-                    "calories": { "value": 518.0 },
-                    "fat": { "value": 26.0 },
-                    "carbs": { "value": 65.0 },
-                    "protein": { "value": 7.0 },
-                    "sodium": { "value": 54.0 },
-                    "calcium": { "value": 12.0 },
-                    "iron": { "value": 6.0 }
+                    "name": "基本功能－DOM操作",
+                    "jquery": "吊炸天",
+                    "backbone": "无,需要jQuery",
+                    "ember": "",
+                    "knockout": "",
+                    "angular": "抄袭了jQuery",
+                    "angular2": "ng1有的都有,没有的也借鉴了react",
+                    "vue": "",
+                    "avalon": "",
+                    "react": "无,我有虚拟DOM"
+                },
+                {
+                    "name": "基本功能－浏览器兼容性",
+                    "jquery": "吊炸天",
+                    "backbone": "无,需要jQuery",
+                    "ember": "",
+                    "knockout": "",
+                    "angular": "IE8,IE9",
+                    "angular2": "IE9",
+                    "vue": "",
+                    "avalon": "",
+                    "react": "IE8"
+                },
+                {
+                    "name": "基本功能－CSS操作",
+                    "jquery": "吊炸天",
+                    "backbone": "无,需要jQuery",
+                    "ember": "",
+                    "knockout": "",
+                    "angular": "ngClass/ngStyle",
+                    "angular2": "ng1有的都有",
+                    "vue": "",
+                    "avalon": "",
+                    "react": "无,我有javascript, 喃尼?"
+                },
+                {
+                    "name": "基本功能－Ajax封装",
+                    "jquery": "吊炸天",
+                    "backbone": "有,但也算是无",
+                    "ember": "",
+                    "knockout": "",
+                    "angular": "吊炸天,$http",
+                    "angular2": "ng1有的都有",
+                    "vue": "",
+                    "avalon": "",
+                    "react": "无,我用Fetch"
+                },
+                {
+                    "name": "基本功能－事件封装",
+                    "jquery": "吊炸天",
+                    "backbone": "强大,基于jQuery代理",
+                    "ember": "",
+                    "knockout": "",
+                    "angular": "吊炸天,声明式语法",
+                    "angular2": "吊炸天,全新声明式语法",
+                    "vue": "",
+                    "avalon": "",
+                    "react": "有,但不是很好用"
+                },
+                {
+                    "name": "基本功能－表单功能",
+                    "jquery": "有,中规中矩,插件无敌",
+                    "backbone": "无,需要jQuery",
+                    "ember": "",
+                    "knockout": "",
+                    "angular": "吊炸天,ngForm写多了也想吐",
+                    "angular2": "ng1有的都有",
+                    "vue": "",
+                    "avalon": "",
+                    "react": "无,还是jQuery思想"
+                },
+                {
+                    "name": "基本功能－内置工具集",
+                    "jquery": "该有的都有,例如each什么的",
+                    "backbone": "比吊炸天还炸裂,underscore",
+                    "ember": "",
+                    "knockout": "",
+                    "angular": "该有的都有",
+                    "angular2": "ng1有的都有",
+                    "vue": "",
+                    "avalon": "",
+                    "react": "无,我有javascript,哦不,是ECMAScript"
+                },
+                {
+                    "name": "基本功能－Promise",
+                    "jquery": "有,defer不好吗",
+                    "backbone": "无,需要jQuery",
+                    "ember": "",
+                    "knockout": "",
+                    "angular": "有,$Q",
+                    "angular2": "ng1有的都有",
+                    "vue": "",
+                    "avalon": "",
+                    "react": "无,我有ES6 Promise"
+                },
+                {
+                    "name": "基本功能－动画功能",
+                    "jquery": "有,很好用,但后来者觉得不好用",
+                    "backbone": "无",
+                    "ember": "",
+                    "knockout": "",
+                    "angular": "吊炸天",
+                    "angular2": "吊炸天,又一次的再一次全新API",
+                    "vue": "",
+                    "avalon": "",
+                    "react": "无,不要拿这点吐槽我,抄袭angular怎么了"
                 }
             ]
         };
 
 
+        angular.forEach($scope.data.frameworkData, function(value, key) {
 
-        $scope.editComment = function (event, dessert) {
+            angular.forEach(value, function(text, textKey) {
+
+                if ($scope.data.textOptions.indexOf(text) === -1) {
+                    $scope.data.textOptions.push(text);
+                }
+            })
+
+        });
+
+
+        $scope.getOptionTypes = function () {
+            return $scope.data.textOptions;
+        };
+
+        $scope.editComment = function (event, item) {
             event.stopPropagation(); // in case autoselect is enabled
 
             var editDialog = {
-                modelValue: dessert.comment,
+                modelValue: item.comment,
                 placeholder: 'Add a comment',
                 save: function (input) {
                     if(input.$modelValue === 'Donald Trump') {
                         return $q.reject();
                     }
                     if(input.$modelValue === 'Bernie Sanders') {
-                        return dessert.comment = 'FEEL THE BERN!'
+                        return item.comment = 'FEEL THE BERN!'
                     }
-                    dessert.comment = input.$modelValue;
+                    item.comment = input.$modelValue;
                 },
                 targetEvent: event,
                 title: 'Add a comment',
@@ -165,15 +202,13 @@ angular.module('demoApp', ['ngMaterial', 'md.data.table'])
             });
         };
 
-        $scope.getTypes = function () {
-            return ['Candy', 'Ice cream', 'Other', 'Pastry'];
-        };
+
 
         $scope.loadStuff = function () {
             $scope.promise = $timeout(function () {
                 // loading
             }, 2000);
-        }
+        };
 
         $scope.logItem = function (item) {
             console.log(item.name, 'was selected');
@@ -182,6 +217,7 @@ angular.module('demoApp', ['ngMaterial', 'md.data.table'])
         $scope.logOrder = function (order) {
             console.log('order: ', order);
         };
+
 
         $scope.logPagination = function (page, limit) {
             console.log('page: ', page);
